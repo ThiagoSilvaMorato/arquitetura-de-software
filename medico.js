42fd4cb3489eae50ce02013670d53a2947dd6df8
Thiago <thiagosmorato@gmail.com>
2025-06-10 10:46:48 -0300

    add Consulta, Exame, and Medico classes to manage medical consultations and exams

---
+++ medico.js
@@ -0,0 +1,10 @@
+class Medico {
+  constructor(id, crm, nome, especalidades, telefone, horarioAtendimento) {
+    this.id = id;
+    this.crm = crm;
+    this.nome = nome;
+    this.especialidades = especalidades;
+    this.telefone = telefone;
+    this.horarioAtendimento = horarioAtendimento;
+  }
+}
